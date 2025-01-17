ba82a5c385d40f1afdf782d185413c563f3a0b48
faydesu <notfayfay@gmail.com>
2025-01-17 20:46:24 +0700

    script.js

--- popangaun/script.js
+++ popangaun/script.js
@@ -13,15 +13,15 @@ popImage.addEventListener('mousedown', () => {
   score++;
   document.getElementById('score').textContent = score;
   localStorage.setItem('popScore', score); // บันทึกคะแนน
-  popImage.src = 'assets/เบบี้องุ่น.png'; // เปลี่ยนภาพเมื่อคลิก
+  popImage.src = 'assets/baby-grape.png'; // เปลี่ยนภาพเมื่อคลิก
 });
 
 // คืนภาพเดิมเมื่อปล่อยคลิก
 popImage.addEventListener('mouseup', () => {
-  popImage.src = 'assets/องุ่น.png';
+  popImage.src = 'assets/grape.png';
 });
 
 // กรณีเลื่อนเมาส์ออกจากภาพ
 popImage.addEventListener('mouseleave', () => {
-  popImage.src = 'assets/องุ่น.png';
+  popImage.src = 'assets/grape.png';
 });
